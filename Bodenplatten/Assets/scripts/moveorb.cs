10acc25fc6cd14194b2c7016ec21393eee0bb17c
SamuelRiesterer <samuel.riesterer@informatik.hs-fulda.de>
2018-08-03 07:02:52 +0200

    Ordnerstruktur wiederhergestellt lol

---
+++ Bodenplatten/Assets/scripts/moveorb.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveorb : MonoBehaviour {
+
+
+		public KeyCode moveL;
+		public KeyCode moveR; //Tasten
+
+		public float horizVel = 0; //In welche Line sich der Orb bewegt
+		public int laneNum = 0; //guckt in welcher Linie sich der Ball befindet & vermeidet überschreitungen vom Feld
+		public string controlLock = "n";
+		public int sternzaehler = 0;
+        public bool tot = false;
+        
+ 
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	//Steuerung in Update weil es immer wieder abfragt!
+	void Update () {
+
+        GetComponent<Rigidbody> ().velocity = new Vector3 (horizVel,0,4);
+
+		if((Input.GetKeyDown (moveL)) && (laneNum>-2) && (controlLock == "n")){
+			horizVel = -2;
+			StartCoroutine (stopSlide());
+			laneNum -= 1;
+			controlLock = "y";
+		}
+
+		if((Input.GetKeyDown (moveR)) && (laneNum<2) && (controlLock == "n")) {
+			horizVel = 2;
+			StartCoroutine (stopSlide());
+			laneNum += 1;
+			controlLock = "y";
+		}
+	}
+
+	// Wenn man das Object mit dem Tag lethal trifft wird das männchen zerstört
+	void OnCollisionEnter(Collision other){
+		if(other.gameObject.tag == "einbrechen"){
+            tot = true;
+            Destroy(other.gameObject);
+            
+
+
+
+        }
+		if(other.gameObject.name == "Stern"){
+			Destroy(other.gameObject);
+			sternzaehler += 1;
+		}
+	}
+
+
+	//Nach einer halben sekunde wird der controllock wieder gelöst
+	//Nach einer halben sekunde wird die bewegung gestoppt
+	IEnumerator stopSlide(){
+		yield return new WaitForSeconds (.5f);
+		horizVel = 0;
+		controlLock = "n";
+	}
+}
