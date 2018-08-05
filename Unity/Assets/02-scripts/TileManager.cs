872d59fe9184063396509465f86b896651dfeff1
EddaSchmidt <edda10@web.de>
2018-08-05 19:22:40 +0200

    infinity mit bruecke am ende wieder gelöscht und random teile der bruecke zsm gewürfelt

--- Unity/Assets/02-scripts/TileManager.cs
+++ Unity/Assets/02-scripts/TileManager.cs
@@ -11,6 +11,7 @@ public class TileManager : MonoBehaviour {
     private float brueckelength = 3.0f;
     private float safeZone = 15.0f;
     private int amnTilesOnScreen = 10;
+    private int lastPrefabIndex = 0;
 
     private List<GameObject> activeTiles;
 
@@ -21,8 +22,15 @@ public class TileManager : MonoBehaviour {
 
         for (int i = 0; i<amnTilesOnScreen; i++)
         {
-            SpawnTile();
-            DeleteTile();
+            // am anfang normale Brücke und danach random brücke
+            if (i<2 )
+            {
+                SpawnTile(0);
+            } else
+            {
+                SpawnTile();
+            }
+            
 
         }
     }
@@ -32,13 +40,22 @@ public class TileManager : MonoBehaviour {
 		if (playerTransform.position.z - safeZone > (spawnZ -amnTilesOnScreen* brueckelength))
         {
             SpawnTile();
+            DeleteTile();
         }
 	}
 
     private void SpawnTile(int prefabIndex = -1) // auf -1 damit
     {
         GameObject go;
-        go = Instantiate(tilePrefabs[0]) as GameObject;
+        if (prefabIndex == -1)
+        {
+            go = Instantiate(tilePrefabs[RandomPrefabIndex()]) as GameObject;
+
+        }
+        else
+        {
+            go = Instantiate(tilePrefabs[prefabIndex]) as GameObject;
+        }
         go.transform.SetParent(transform);
         go.transform.position = Vector3.forward * spawnZ; //bruecke vorwärts machen
         spawnZ += brueckelength;
@@ -49,5 +66,19 @@ public class TileManager : MonoBehaviour {
     {
         Destroy(activeTiles[0]);
         activeTiles.RemoveAt(0);
+    }
+
+    private int RandomPrefabIndex()
+    {
+        if (tilePrefabs.Length <= 1)
+            return 0;
+
+        int randomIndex = lastPrefabIndex;
+        while(randomIndex == lastPrefabIndex)
+        {
+            randomIndex = Random.Range(0, tilePrefabs.Length);
+        }
+        lastPrefabIndex = randomIndex;
+        return randomIndex;
     }
 }
