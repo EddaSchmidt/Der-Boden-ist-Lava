d60bed4b25fef1f4ec5f80905bf60d38e1654508
SamuelRiesterer <samuel.riesterer@informatik.hs-fulda.de>
2018-08-17 17:03:58 +0200

    Springen

--- Unity/Assets/02-scripts/CharacterMovement.cs
+++ Unity/Assets/02-scripts/CharacterMovement.cs
@@ -5,6 +5,86 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour {
 
     public GameObject player;
+
+    public Transform startPos;
+    public Transform endPos;
+    public bool repeatable = false;
+    public float speed = 0.5f;
+    public float lerpTime = 2.0f;
+
+    private float startTime;
+    private float totalDistance;
+
+    IEnumerator Start() {
+        startTime = Time.time;
+        totalDistance = Vector3.Distance(startPos.position, endPos.position);
+
+        while (repeatable) {
+            yield return RepeatLerp(startPos.position, endPos.position, lerpTime);
+            yield return RepeatLerp(endPos.position, startPos.position, lerpTime);
+        }
+    }
+
+    void Update () {
+        if(!repeatable) {
+            float currentDuration = (Time.time - startTime) * speed;
+            float journeyFraction = currentDuration / totalDistance;
+            player.transform.position = Vector3.Lerp(startPos.position, endPos.position, journeyFraction);
+        }
+    }
+
+    public IEnumerator RepeatLerp(Vector3 a, Vector3 b, float time) {
+        float i = 0;
+        float rate = (1f / time) * speed;
+        while (i < 1f) {
+            i = i + Time.deltaTime * rate;
+            player.transform.position = Vector3.Lerp(a, b, i);
+            yield return null;
+        }
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /* public GameObject player;
     public Vector3 startPos;
     public Vector3 endPos;
     public float distance = 1.0f;
@@ -32,5 +112,5 @@ public class CharacterMovement : MonoBehaviour {
             
         
        
-    }
+    } */
 }
