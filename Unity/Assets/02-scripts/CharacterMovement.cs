9957e1d6c167dc8c7434c3d9fc17bc0023f7ac16
SamuelRiesterer <samuel.riesterer@informatik.hs-fulda.de>
2018-08-17 16:38:12 +0200

    Springen Test

---
+++ Unity/Assets/02-scripts/CharacterMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+    public GameObject player;
+    public Vector3 startPos;
+    public Vector3 endPos;
+    public float distance = 1.0f;
+    public float lerpTime = 1f;
+    private float currentLerpTime = 0;
+    private bool keyHit = false;
+    private bool isMoving = false;
+
+
+	// Use this for initialization
+	void Start () {
+        startPos = player.transform.position;
+        endPos = player.transform.position + Vector3.left * distance;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        
+
+        if (Input.GetKeyDown(KeyCode.A) && keyHit == false) {
+                player.transform.position = Vector3.Lerp(startPos, endPos, lerpTime * Time.deltaTime);
+                
+        }
+            
+        
+       
+    }
+}
