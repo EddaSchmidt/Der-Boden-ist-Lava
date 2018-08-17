0a060c8c1268e166606d983d33e5469848fd0b95
SamuelRiesterer <samuel.riesterer@informatik.hs-fulda.de>
2018-08-17 15:08:03 +0200

    gajlkj

--- Unity/Assets/02-scripts/PlayerMotor.cs
+++ Unity/Assets/02-scripts/PlayerMotor.cs
@@ -67,7 +67,7 @@ public class PlayerMotor : MonoBehaviour {
         //X - Left and Right
         moveVector.x = Input.GetAxisRaw("Horizontal") * speed; //Spieler kann rechts und links gehen
         //Y - Up and Down
-        moveVector.y = verticalVelocity;
+        moveVector.y = verticalVelocity * Time.deltaTime;
         //Z - Foward and Backward
         moveVector.z = speed; 
 
